c9885096795b3157172bdd88e8ee71e6ddd89f3d
davian.solera <davian.solera@onelinkbpo.com>
2024-11-26 10:30:03 -0500

    Add ILogger

--- MS_Test_Fullstack/Services/ManagerAirportsService.cs
+++ MS_Test_Fullstack/Services/ManagerAirportsService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 using MS_Test_Fullstack.Domain.Interfaces;
 using MS_Test_Fullstack.Domain.IReposotories;
 using MS_Test_Fullstack.Domain.Models;
@@ -8,9 +9,10 @@ namespace MS_Test_Fullstack.Services
 {
     public class ManagerAirportsService: IManagerAirports
     {
+        private readonly ILogger<FlightsServices> _logger;
         private readonly IAirportsRepository _airportsRepository;
-        public ManagerAirportsService(IAirportsRepository  airportsRepository){
-
+        public ManagerAirportsService(ILogger<FlightsServices> logger, IAirportsRepository  airportsRepository){
+            _logger = logger;
             _airportsRepository = airportsRepository;
         }
 
@@ -18,7 +20,7 @@ namespace MS_Test_Fullstack.Services
         {
             try
             {
-
+                _logger.LogInformation("service processed a request GetAirports.");
                 IEnumerable<IataCodes> airports = await _airportsRepository.GetAirports(); 
 
 
@@ -26,6 +28,7 @@ namespace MS_Test_Fullstack.Services
             }
             catch (Exception ex)
             {
+                _logger.LogError(ex, "An unexpected error occurred: {Message}", ex.Message);
                 return Result<IEnumerable<IataCodes>>.Failure(ex.Message);
             }
 
